f542b7ebbb5ecd80561b6fa266fccebead083ed6
mmdctjj <jiangpengjie@bizseer.com>
2023-04-29 21:57:43 +0800

    fix: fix config

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   apiParser: {},
+  base: 'raetable',
   resolve: {
     docDirs: ['docs'],
     atomDirs: [{ type: 'component', dir: 'src/components' }],
     entryFile: './src/index.ts',
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '/raetable/' : '/',
   themeConfig: {
     name: 'raetable',
     prefersColor: { default: 'auto' },
