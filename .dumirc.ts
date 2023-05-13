9bfffe1b559c5d6bae651313d5958948595ba470
mmdctjj <jiangpengjie@bizseer.com>
2023-05-13 19:13:36 +0800

    docs: 完善组件文档配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,22 +1,29 @@
 import { defineConfig } from 'dumi';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   outputPath: 'docs-dist',
   apiParser: {},
   base: 'raetable',
+  favicons: [isProd ? '/raetable/favicon.ico' : '/favicon.ico'],
+  metas: [
+    { name: 'keywords', content: 'raetable' },
+    { name: 'description', content: 'react antd Easy Table' },
+  ],
   resolve: {
     docDirs: ['docs'],
     atomDirs: [{ type: 'component', dir: 'src/components' }],
     entryFile: './src/index.ts',
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/raetable/' : '/',
+  publicPath: isProd ? '/raetable/' : '/',
   themeConfig: {
-    logo: process.env.NODE_ENV === 'production' ? '/raetable/logo.png' : '/logo.png',
+    logo: isProd ? '/raetable/logo.png' : '/logo.png',
     name: 'raetable',
     prefersColor: { default: 'auto' },
     footer: '@raetable',
     socialLinks: {
-      github: 'https://github.com/mmdctjj/raetable'
+      github: 'https://github.com/mmdctjj/raetable',
     },
   },
 });
